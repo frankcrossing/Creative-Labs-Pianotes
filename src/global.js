5c6eb9ebbc5edd6e4d64ec0108d09843485c6b5e
Frank Xing <frankcrossing@g.ucla.edu>
2019-08-31 15:13:02 -0700

    Add files via upload

---
+++ src/global.js
@@ -0,0 +1,41 @@
+global.startFlag = true;
+global.startRest = false;
+global.composer = `C: 
+`;
+global.measure = `M: 4/4
+`;
+global.measureUpdated = `M: 4/4
+`;
+global.length = `L: 1/16
+`;
+global.metronome = `Q:1/4=100
+`;
+global.clef = `V: V1 clef=treble
+`;
+
+global.beat_count = 0;
+
+
+global.noteStart = `[V: V1] `;
+global.notes = `a2b2b2`;
+global.copy = ``;
+global.noteEnd = ` |]
+`;
+
+global.measure_num = 0;
+
+
+global.notation = 
+	global.composer +
+	global.measure +
+	global.length +
+	global.metronome +
+	global.clef +
+	global.noteStart +
+	global.notes +
+	global.noteEnd;
+
+
+
+
+
